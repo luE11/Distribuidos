049d92209021223f81cea244ca026c89ed5cb80b
Luis Martinez <luisedu555@gmail.com>
2020-11-14 17:48:57 -0500

    Servicio que muestra una lista de recetas, implementado! :D

--- index.js
+++ index.js
@@ -3,10 +3,27 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/recipes', (req, res) => {
+  var data = readRecipes()
+  res.send(data)
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Server listening at http://localhost:${port}`)
 })
+
+/**
+ * Reads the recipes.json file and save the data into a string variable
+ * that is returned
+ */
+function readRecipes(){
+  let json = require('./data/recipes.json')
+  let jsonArray = json.recipes
+  var dataRec = 'Recipe List :)</br>'
+  for (let i = 0; i < jsonArray.length; i++) {
+    dataRec+='Id: '+jsonArray[i].id+' Recipe Name: '+jsonArray[i].recipe_name
+              +' Preparation Time: '+jsonArray[i].preparation_time+' Type: '
+                  +jsonArray[i].preparation_time+((i<jsonArray.length-1)?'</br>':'')
+  }
+  return dataRec;
+}
